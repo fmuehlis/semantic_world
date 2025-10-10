00102f47f9a4b544d2a67683ac9408c6ad0d436f
fmuehlis <fmuehlis@posteo.de>
2025-10-10 09:35:54 +0200

    fix: tests not catching missing body index after serialization

--- test/test_body/test_bodies.py
+++ test/test_body/test_bodies.py
@@ -4,9 +4,10 @@ import unittest
 import trimesh.boolean
 
 from semantic_world.adapters.mesh import STLParser
-from semantic_world.world_description.geometry import Box
 from semantic_world.datastructures.prefixed_name import PrefixedName
 from semantic_world.spatial_types.spatial_types import TransformationMatrix
+from semantic_world.world import World
+from semantic_world.world_description.geometry import Box
 from semantic_world.world_description.shape_collection import ShapeCollection
 from semantic_world.world_description.world_entity import Body
 
@@ -19,6 +20,10 @@ class JSONTestCase(unittest.TestCase):
             Box(origin=TransformationMatrix.from_xyz_rpy(0, 1, 0, 0, 0, 1, body))
         ]
         body.collision = ShapeCollection(collision, reference_frame=body)
+
+        world = World()
+        with world.modify_world():
+            world.add_kinematic_structure_entity(body)
 
         json_data = body.to_json()
         body2 = Body.from_json(json_data)
